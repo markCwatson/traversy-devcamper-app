016895754f945c785e56a591cbb886a9b9671d82
Mark Watson <markus.c.watson@gmail.com>
2022-10-05 17:14:03 -0300

    setup logger middleware with morgan

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import morgan from 'morgan'
 
 import { router as schools }  from './routes/schools.js'
 
 const app = express()
 
+if (process.env.NODE_ENV === 'dev')
+{
+    // Must setup middleware before route definitions
+    app.use(morgan('dev'))
+}
+
+// Setup routes
 app.use('/api/v1/schools', schools)
 
 app.listen(process.env.PORT, () => {
