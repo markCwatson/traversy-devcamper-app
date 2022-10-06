b801d247824d0c24c02e42f18fce6f7f82353c2a
Mark Watson <markus.c.watson@gmail.com>
2022-10-06 19:20:55 -0300

    trying to make asyncHandler more clear

--- middleware/async.js
+++ middleware/async.js
@@ -1,7 +1,8 @@
 // @desc    Used to replace try/catch blocks in routes. 
 //          asyncHandler receives a function (callback) and returns a function with params req, res, and next.
-const asyncHandler = (callback) => (req, res, next) => { 
-    Promise.resolve(callback(req, res, next)).catch(next)
-}
+const asyncHandler = (callback) => { 
+    return (req, res, next) => { 
+        Promise.resolve(callback(req, res, next)).catch(next)
+}}
 
 export { asyncHandler }
