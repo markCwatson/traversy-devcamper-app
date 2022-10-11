3bd3d3cf8cebe4cb7ee0024d0259c62bb565f68f
Mark Watson <mwatson@prosaris.ca>
2022-10-11 09:52:11 -0300

    Create professor route

--- controllers/professors.js
+++ controllers/professors.js
@@ -1,11 +1,12 @@
 import { Professor } from '../models/professor.js'
+import { School } from '../models/school.js'
 import { ErrorResponse } from '../utils/errorResponse.js'
 import { asyncHandler } from '../middleware/async.js'
 
 
 // @desc    Get all professors in database.
 // @route   GET /api/v1/professors
-// @route   GET /api/v1/schools/:schoolId/courses
+// @route   GET /api/v1/schools/:schoolId/professors
 // @access  Public
 const getProfessors = asyncHandler(async (req, res, next) => {
     let query
@@ -28,4 +29,44 @@ const getProfessors = asyncHandler(async (req, res, next) => {
     })
 })
 
-export { getProfessors }
+// @desc    Get a single professor in database.
+// @route   GET /api/v1/professors/:id
+// @access  Public
+const getProfessor = asyncHandler(async (req, res, next) => {
+    const prof = await Professor.findById(req.params.id).populate({
+        path: 'school',
+        select: 'name location.formattedAddress'
+    })
+    
+    if (!prof) {
+        return next(new ErrorResponse('No professor found!', 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        data: prof
+    })
+})
+
+// @desc    Add a new professor to database.
+// @route   POST /api/v1/schools/:schoolId/professors
+// @access  Private
+const addProfessor = asyncHandler(async (req, res, next) => {
+    // Add this professor to a school using the field in professor model
+    req.body.school = req.params.schoolId
+
+    const school = await School.findById(req.params.schoolId)
+    
+    if (!school) {
+        return next(new ErrorResponse('No school found!', 404))
+    }
+
+    const prof = await Professor.create(req.body)
+
+    res.status(200).json({
+        success: true,
+        data: prof
+    })
+})
+
+export { getProfessors, getProfessor, addProfessor }
--- routes/professors.js
+++ routes/professors.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
-import { getProfessors } from '../controllers/professors.js'
+import { getProfessors, getProfessor, addProfessor } from '../controllers/professors.js'
 
 const router = express.Router({ mergeParams: true })
 
 router.route('/').get(getProfessors)
+router.route('/').post(addProfessor)
+router.route('/:id').get(getProfessor)
 
 export { router }
